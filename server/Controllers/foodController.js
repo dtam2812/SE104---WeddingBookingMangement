89f4cfafb65ebf371b5190e12133100b708dffc8
Dinh Nguyen Duc Tam <23521384@gm.uit.edu.vn>
2026-05-08 04:07:36 +0700

    foodController added

---
+++ server/Controllers/foodController.js
@@ -0,0 +1,37 @@
+import { Food } from "../Models/index.js";
+
+export const getAll = async (req, res) => {
+  try {
+    const data = await Food.find();
+    res.json({ data });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const doc = await Food.create(req.body);
+    res.json({ success: true, id: doc.id });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    await Food.findByIdAndUpdate(req.params.id, req.body);
+    res.json({ success: true });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    await Food.findByIdAndDelete(req.params.id);
+    res.json({ success: true });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
